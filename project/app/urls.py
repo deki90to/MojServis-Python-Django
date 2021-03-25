0dd3ca7dc4b45fa6f73f3c8b4ae6a7a7a5536c79
admin <admin@gmail.comgit config --global user.name admingit commit -m .git config --global user.email admin@gmail.com>
2021-03-25 19:53:16 +0100

    update/delete

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('new/', views.new, name='new'),
+	path('<pk>/update/', views.update, name='update'),
+	path('<pk>/delete/', views.delete, name='delete'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -19,3 +19,22 @@ def new(request):
 			return redirect('index')
 
 	return render(request, 'new.html', {'mali_servis':mali_servis, 'form':form})
+
+def update(request, pk):
+	mali_servis = MaliServis.objects.get(pk=pk)
+	form = MaliServisForm(instance=mali_servis)
+
+	if request.method == 'POST':
+		form = MaliServisForm(request.POST, instance=mali_servis)
+		if form.is_valid():
+			form.save()
+			return redirect('index')
+
+	return render(request, 'update.html', {'mali_servis':mali_servis, 'form':form})
+
+def delete(request, pk):
+	mali_servis = MaliServis.objects.get(pk=pk)
+
+	if request.method == 'POST':
+		mali_servis.delete()
+		return redirect('index')
