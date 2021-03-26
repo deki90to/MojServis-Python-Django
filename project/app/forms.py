156477e1b1b0ae0e87f084af4f422cb7fea71478
Dejan Jovanovic <deki90to@gmail.com>
2021-03-26 18:40:43 +0100

    lg buttons

--- project/app/forms.py
+++ project/app/forms.py
@@ -25,25 +25,8 @@ class MaliServisForm(forms.ModelForm):
 		
 		widgets = {
 			'user':forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'admin', 'type':'hidden'}),
-			# 'ms_automobila':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Marka'}),
 			'tablica':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Tablica'}),
 			'kilometraza':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Kilometraza'}),
 			'ulje':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Ulje'}),
 			'info':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Vise informacija'}),
 		}
-
-
-
-# class VelikiServisForm(forms.ModelForm):
-# 	class Meta:
-# 		fields = (
-# 			'vs_automobila',
-# 			'tablica',
-# 			'kilometraza',
-# 			'kais',
-# 			'lanac',
-# 			'zatezac',
-# 			'roler',
-# 			'spaner',
-# 			'pumpa_vode',
-# 			'info')
